1d516cab8bfd0f954d71771fef65f075268747e9
umlai <umlai@LAPTOP-M76VSPGE.hub>
2020-09-14 14:40:25 +1200

    Print My favourite mode of transportation

--- PartD/src/Assignment2PartD.java
+++ PartD/src/Assignment2PartD.java
@@ -4,6 +4,8 @@ public class Assignment2PartD {
 	public static void main(String[] args) {
 		System.out.println("Hi, my name is Umme Laila");
 		System.out.println("My favourite colour is Black");
+		System.out.println("My favourite mode of transportation is Car");
+
 	}
 
 }
