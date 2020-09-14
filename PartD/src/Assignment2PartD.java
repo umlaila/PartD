0d8dccded20ece19f5b1de44a9ce00ab87a01b6d
umlai <umlai@LAPTOP-M76VSPGE.hub>
2020-09-14 14:41:56 +1200

    Print The best day of the week

--- PartD/src/Assignment2PartD.java
+++ PartD/src/Assignment2PartD.java
@@ -5,6 +5,7 @@ public class Assignment2PartD {
 		System.out.println("Hi, my name is Umme Laila");
 		System.out.println("My favourite colour is Black");
 		System.out.println("My favourite mode of transportation is Car");
+		System.out.println("The best day of the week is Friday");
 
 	}
 
