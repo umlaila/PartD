39d2db578a15b5e3eb4505792678ddbcc8ef8041
umlai <umlai@LAPTOP-M76VSPGE.hub>
2020-09-14 14:38:21 +1200

    Print My favourite colour

--- PartD/src/Assignment2PartD.java
+++ PartD/src/Assignment2PartD.java
@@ -3,7 +3,7 @@ public class Assignment2PartD {
 
 	public static void main(String[] args) {
 		System.out.println("Hi, my name is Umme Laila");
-
+		System.out.println("My favourite colour is Black");
 	}
 
 }
